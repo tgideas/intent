9a40c7b87b75fb5e1a2f898715a67c8597030f30
mamboer <mamboer@live.com>
2014-10-16 14:02:57 +0800

    init

---
+++ assets/data/intent.js
@@ -0,0 +1,34 @@
+var data = [
+    {
+        img: 'https://avatars2.githubusercontent.com/u/1005746?v=2&s=96',
+        title: 'Bruce Wan',
+        text: 'TODO'
+    },
+    {
+        img: 'https://avatars1.githubusercontent.com/u/4222763?v=2&s=96',
+        title: 'Kimi Xiao',
+        text: 'TODO'
+    },
+    {
+        img: 'https://avatars0.githubusercontent.com/u/8232739?v=2&s=96',
+        title: '七宝',
+        text: 'TODO'
+    },
+    {
+        img: 'https://avatars0.githubusercontent.com/u/3890675?v=2&s=96',
+        title: 'Shine',
+        text: 'TODO'
+    },
+    {
+        img: 'https://avatars1.githubusercontent.com/u/3692129?v=2&s=96',
+        title: 'Nornor',
+        text: 'TODO'
+    }   
+    ,{
+        img: 'https://avatars1.githubusercontent.com/u/3829334?v=2&s=96',
+        title: 'Piggy',
+        text: 'TODO'
+    }   
+];
+
+
